e1fed2f161af71c77c6c24a2b4a8c462cc766b33
Yi Li <yil8@uci.edu>
2016-02-01 15:15:49 -0800

    chromdeep/seq2npy.py: line_num bug fixed

--- chromdeep/seq2npy.py
+++ chromdeep/seq2npy.py
@@ -6,15 +6,16 @@ import numpy as np
 
 from chromdeep.utils import *
 
-# TARGET = {'BR':0, 'O':1, 'B':2, 'DG':3}
+# TARGET = {'BR':0, 'O':1, 'B':2, 'G':3}
 SEQ_LEN = 1000
 
 def main():
     infile = open(sys.argv[1])
     outbase = sys.argv[2]
-    color_str = sys.argv[3] #'BR,O,B,DG'
-    line_num = int(sys.argv[4])
-    
+    color_str = sys.argv[3] #'BR,B,O,G'
+
+    lines = infile.readlines()
+    line_num = len(lines)
     color_lst = color_str.split(',')
     color_dict = {}
     class_num = len(color_lst)
@@ -25,8 +26,8 @@ def main():
     X = np.zeros((line_num, SEQ_LEN, 4), dtype='float32')
     Y = np.zeros((line_num, class_num), dtype='float32')
     
-    i = 0
-    for line in infile:
+    for i in range(0, line_num):
+        line = lines[i]
         chrom, start, end, label, color, seq = line.strip('\n').split('\t')
         x = onehot(seq)
         y_idx = color_dict[color]
