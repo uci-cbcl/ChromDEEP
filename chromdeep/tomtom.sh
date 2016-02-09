370399a6e430a86415a687ff2c2e5072a3b9f90f
Yi Li <yil8@uci.edu>
2016-02-08 17:44:49 -0800

    chromdeep/tomtom.sh: bug fixed

--- chromdeep/tomtom.sh
+++ chromdeep/tomtom.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-~/Programs/MEME/meme_4.11.1/bin/tomtom -no-ssc -verbosity 2 -min-overlap 5 -dist pearson -evalue -thresh 0.01 ~/Programs/MEME/meme_4.11.1/db/jolma2013.meme ~/Programs/MEME/meme_4.11.1/db/JASPAR_CORE_2014_vertebrates.meme ~/Programs/MEME/meme_4.11.1/db/uniprobe_mouse.meme $1 -oc .
+~/Programs/MEME/meme_4.11.1/bin/tomtom -no-ssc -oc . -verbosity 2 -min-overlap 5 -dist pearson -evalue -thresh 0.01 $1 ~/Programs/MEME/meme_4.11.1/db/jolma2013.meme ~/Programs/MEME/meme_4.11.1/db/JASPAR_CORE_2014_vertebrates.meme ~/Programs/MEME/meme_4.11.1/db/uniprobe_mouse.meme
 
